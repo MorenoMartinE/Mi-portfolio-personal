2aa0a528f64791f395c14fb3b91108df395e3cea
Martin Moreno <102624788+MorenoMartinE@users.noreply.github.com>
2022-07-06 10:14:19 -0300

    Update config.js

--- app/helpers/config.js
+++ app/helpers/config.js
@@ -21,14 +21,18 @@ export const DATOS_MENU = {
 export const API_GITHUB_URLs = {
     urlUsr : "https://api.github.com/users/MorenoMartinE",
     urlRep : "https://api.github.com/users/MorenoMartinE/repos",
-    numReps : 3,
+    numReps : 4,
 }
 
+
 export const REPOS_IMG_URL = [
+    
     "https://repository-images.githubusercontent.com/485933542/7a42a097-64f2-4338-9b9e-1c3cd2ee289b",
     "https://repository-images.githubusercontent.com/485933542/7a42a097-64f2-4338-9b9e-1c3cd2ee289b",
+    "",
     "https://repository-images.githubusercontent.com/479137070/06e80da9-57e0-4312-81a5-afbc35777b57",
     "https://repository-images.githubusercontent.com/510566718/89cf833e-9a6d-49a9-ae97-0878689e8210"
+    
 ]
 
 
